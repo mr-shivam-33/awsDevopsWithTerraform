364e6a44ff460588cb00d58f1c495b9f0b2d5110
Shivam Pandey <106185966+mr-shivam-33@users.noreply.github.com>
2023-07-18 21:43:42 +0530

    Update aws-provider.tf
    key

--- try-terra/aws-provider.tf
+++ try-terra/aws-provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIA4CMJKB6YF4A7LFGW"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
